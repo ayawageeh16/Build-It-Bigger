1db57e58e7e1d3c53ca11bc5c2a26f70dd8e9fd7
ayawageeh16 <ayawageeh16@gmail.com>
2018-06-03 09:35:01 +0200

    last modification

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,25 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.udacity.gradle.builditbigger.R;
+
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main_activity, container, false);
+        return root;
+    }
+}
